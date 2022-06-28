fd6b03d3778f479813a854ac09d8159548605e65
SergeyOstapenko <sergiy.v.ostapenko@gmail.com>
2022-06-28 22:20:26 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
+import './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+
+const input = document.querySelector('#search-box');
+const list = document.querySelector('.country-list');
+const div = document.querySelector('.country-info');
