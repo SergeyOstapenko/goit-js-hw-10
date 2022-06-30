bbea5f6d4703ea5680f40ac8c7d5ad603a1ea588
SergeyOstapenko <sergiy.v.ostapenko@gmail.com>
2022-06-30 19:00:29 +0300

    task1

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,6 @@
-export default fetchCountries
-
-function fetchCountries(name) {
-
+import Notiflix from 'notiflix';
+export default function fetchCountries(name) {
+  const BASE_URL = 'https://restcountries.com/v2/name/';
+  const searchUrl = '?fields=name,capital,population,flags,languages';
+  return fetch(`${BASE_URL}${name}${searchUrl}`);
 }
